6c726b380a3b0d563bf2780a06c54f81e85c8a8a
李鹏跃 <702029772@qq.com>
2018-11-29 19:32:56 +0800

    【完成demo搭建】

--- Example/PYGradientView/PYViewController.m
+++ Example/PYGradientView/PYViewController.m
@@ -7,23 +7,81 @@
 //
 
 #import "PYViewController.h"
-
+#import <PYGradientView/PYGradientView.h>
 @interface PYViewController ()
-
+@property (nonatomic,strong) PYGradientView *gradientView;
 @end
 
 @implementation PYViewController
 
-- (void)viewDidLoad
-{
+- (void)viewDidLoad {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    self.gradientView = [PYGradientView new];
+    CGRect gradientViewFrame = self.view.bounds;
+//    gradientViewFrame.size.height -= 60;
+    self.gradientView.frame = gradientViewFrame;
+    [self.view addSubview:self.gradientView];
+    
+    
+    UIButton *button = [UIButton new];
+    UIButton *button2 = [UIButton new];
+    
+    CGRect buttonFrame = CGRectMake(20, gradientViewFrame.size.height - 60,100, 40);
+    button.frame = buttonFrame;
+    buttonFrame.origin.x += 140;
+    button2.frame = buttonFrame;
+    [self.view addSubview:button];
+    [self.view addSubview:button2];
+    
+    [button setTitle:@"线性渲染" forState:UIControlStateNormal];
+    [button2 setTitle:@"扩散渲染" forState:UIControlStateNormal];
+    
+    [button setTitleColor:UIColor.whiteColor forState:UIControlStateNormal];
+    [button2 setTitleColor:UIColor.whiteColor forState:UIControlStateNormal];
+    
+    button.layer.cornerRadius = 6;
+    button.layer.borderWidth = 1;
+    button.layer.borderColor = UIColor.whiteColor.CGColor;
+    
+    button2.layer.cornerRadius = 6;
+    button2.layer.borderWidth = 1;
+    button2.layer.borderColor = UIColor.whiteColor.CGColor;
+    
+    [button addTarget:self action:@selector(clickLineButton) forControlEvents:UIControlEventTouchUpInside];
+    [button2 addTarget:self action:@selector(clickRadiusButton) forControlEvents:UIControlEventTouchUpInside];
+    return;
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void) clickLineButton {
+        [self.gradientView drawLineGradient:^(PYGradientViewLineConfig *lineConfig) {
+            lineConfig
+            .setUpScaleEndCenter(CGPointMake(0, 0))
+            .setUpScaleStartCenter(CGPointMake(1, 1))
+            .setUpColorArray(@[
+                               UIColor.redColor,
+                               UIColor.cyanColor
+                               ])
+            .setUpLocationArray(@[
+                                  @0,@1
+                                  ]);
+    
+        }];
+}
+- (void) clickRadiusButton {
+    [self.gradientView drawRadialGradient:^(PYGradientViewDrawRadialConfig *radialConfig) {
+        radialConfig
+        .setUpScaleEndCenter(CGPointMake(0.5, 0.5))
+        .setUpScaleStartCenter(CGPointMake(0.5, 0.5))
+        .setUpColorArray(@[
+                           UIColor.redColor,
+                           UIColor.blueColor
+                           ])
+        .setUpStartRadius(0)
+        .setUpEndRadius(1000)
+        .setUpLocationArray(@[@0.1,@1]);
+    }];
+    
 }
 
 @end
